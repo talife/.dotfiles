3229097376099e7eb57d809890c7c8116c0f41dc
talife <gilletvincent@gmail.com>
2024-06-05 11:57:35 +0800

    dding ctrl-v for paste from clipbaord

--- nvim/.config/nvim/lua/talife/remap.lua
+++ nvim/.config/nvim/lua/talife/remap.lua
@@ -20,6 +20,8 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
+vim.keymap.set("n", "<C-v>", '"+p')
+
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
