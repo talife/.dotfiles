47ffbaa50d9d004a8ca41cbef0e2563408b44b4c
Vincent Gillet <gilletvincent@gmail.com>
2026-07-02 11:16:57 +0800

    switch swap-param locally to github repo

--- nvim/.config/nvim/lua/talife/lazy/swap-param.lua
+++ nvim/.config/nvim/lua/talife/lazy/swap-param.lua
@@ -1,5 +1,5 @@
 return {
-    dir = "/home/talife/git/swap-param",
+    "talife/swap-param",
     config = function()
         local sp = require("swap-param")
         vim.keymap.set("n", "<leader>fr", sp.swap_with_next, { desc = "Global Parameter Swap" })
